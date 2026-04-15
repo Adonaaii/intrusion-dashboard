11e4423cf626147c34c304d6fe0670eb87a4e436
Samuel Asante <134442052+Adonaaii@users.noreply.github.com>
2026-04-15 12:51:45 +0000

    b2

--- analyzer.py
+++ analyzer.py
@@ -18,4 +18,39 @@ if __name__ == "__main__":
     print(len(logs))        # how many logs
     print(logs[:5])         # first 5 logs
 
+def parse_logs(log_lines):
+    parsed_logs = []
+    for line in log_lines:
+        try:
+            start = line.find("[")
+            end = line.find("]")
+            time = line[start+1:end]
+            
+
+            first_quote = line.find('"')
+            second_quote = line.find('"', first_quote+1)
+            request = line[first_quote+1:second_quote]
+            
+            line_no_time = line[:start] + line[end+1:]
+            
+            first_quote = line_no_time.find('"')
+            second_quote = line_no_time.find('"', first_quote + 1)
+            
+            clean_line = line_no_time[:first_quote] + line_no_time[second_quote+1:]
+            parts = clean_line.split()
+
+
+            print("RAW LINE:", line)
+            print("TIMESTAMP:", time)
+            print("REQUEST:", request)
+            print("CLEAN LINE:", clean_line)
+            print("PARTS:", parts)
+            
+            
+        except:
+            print("something went wrong")
+
+            
+parse_logs(['192.168.1.25 - - [10/Jan/2026:18:01:17] "POST /login HTTP/1.1" 401 128'])
+
 
